6c736b49444a60da45e6f72a673b546d2248f0f1
shr0mi <shromi3.14@gmail.com>
2025-12-16 12:50:11 +0600

    Implement book rendering functionality to display library contents in the DOM

--- script.js
+++ script.js
@@ -17,3 +17,45 @@ const book5 = new Book("Pride and Prejudice", "Jane Austen", "Romance, Classic")
 myLibrary.push(book1, book2, book3, book4, book5);
 
 console.log(myLibrary);
+
+// Render Books to DOM
+const books = document.querySelector(".books");
+
+function renderBooks(){
+    // First clear existing books
+    books.innerHTML = "";
+
+    for(let book of myLibrary){
+        // Create Card
+        const card = document.createElement("div");
+        card.classList.add("card");
+
+        // Create Image
+        const img = document.createElement("img");
+        img.src = "no-image-found.png";
+        img.alt = "book cover";
+        card.appendChild(img);
+
+        // Create Info
+        const info = document.createElement("div");
+        info.classList.add("info");
+
+        const title = document.createElement("h4");
+        title.textContent = book.title;
+        info.appendChild(title);
+
+        const writer = document.createElement("p");
+        writer.textContent = `Author: ${book.writer}`;
+        info.appendChild(writer);
+
+        const genre = document.createElement("p");
+        genre.textContent = `Genre: ${book.genre}`;
+        info.appendChild(genre);
+
+        card.appendChild(info);
+        books.appendChild(card);
+    }
+}
+
+renderBooks();
+
